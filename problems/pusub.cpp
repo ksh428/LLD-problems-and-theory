efa14c1893459f9a81cfa990874e764ca0c7c5dd
KSHOUNISH CHAKRABORTY <kshounish03@gmail.com>
2026-04-30 18:22:19 +0530

    pubsub better

--- problems/pusub.cpp
+++ problems/pusub.cpp
@@ -1,4 +1,8 @@
 #include <bits/stdc++.h>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+
 using namespace std;
 
 class Message {
@@ -21,16 +25,62 @@ public:
 
 class Topic {
     string name;
+
     vector<Subscriber*> subscribers; // non-owning
 
+    queue<Message> q;
+    mutex mtx;
+    condition_variable cv;
+
+    thread worker;
+    bool stop = false;
+
+    // worker thread function
+    void process() {
+        while (true) {
+            unique_lock<mutex> lock(mtx);
+
+            cv.wait(lock, [&]() {
+                return !q.empty() || stop;
+            });
+
+            if (stop && q.empty()) return;
+
+            Message msg = q.front();
+            q.pop();
+
+            auto subs_copy = subscribers; // copy under lock
+            lock.unlock();
+
+            // process outside lock
+            for (auto sub : subs_copy) {
+                sub->consume(msg);
+            }
+        }
+    }
+
 public:
-    Topic(const string& n) : name(n) {}
+    Topic(const string& n) : name(n) {
+        worker = thread(&Topic::process, this);
+    }
+
+    ~Topic() {
+        {
+            lock_guard<mutex> lock(mtx);
+            stop = true;
+        }
+        cv.notify_all();
+        if (worker.joinable()) worker.join();
+    }
 
     void subscribe(Subscriber* s) {
+        lock_guard<mutex> lock(mtx);
         subscribers.push_back(s);
     }
 
     void unsubscribe(Subscriber* s) {
+        lock_guard<mutex> lock(mtx);
+
         vector<Subscriber*> temp;
         for (auto it : subscribers) {
             if (it != s)
@@ -40,9 +90,11 @@ public:
     }
 
     void publish(const Message& msg) {
-        for (auto it : subscribers) {
-            it->consume(msg);
+        {
+            lock_guard<mutex> lock(mtx);
+            q.push(msg);
         }
+        cv.notify_one();
     }
 };
 
@@ -50,29 +102,52 @@ class Pubsubsystem {
     unordered_map<string, unique_ptr<Topic>> topics;
     unordered_map<string, unique_ptr<Subscriber>> subscribers;
 
+    mutex topic_mutex;
+    mutex subscriber_mutex;
+
 public:
     void createtopic(const string& name) {
+        lock_guard<mutex> lock(topic_mutex);
         if (topics.find(name) != topics.end()) return;
         topics[name] = make_unique<Topic>(name);
     }
 
     void createsubscriber(const string& name) {
+        lock_guard<mutex> lock(subscriber_mutex);
         if (subscribers.find(name) != subscribers.end()) return;
         subscribers[name] = make_unique<Subscriber>(name);
     }
 
     void subscribe(const string& topic, const string& sub) {
-        if (topics.find(topic) == topics.end()) return;
-        if (subscribers.find(sub) == subscribers.end()) return;
+        Topic* t = nullptr;
+        Subscriber* s = nullptr;
 
-        topics[topic]->subscribe(subscribers[sub].get());
+        {
+            lock_guard<mutex> lock(topic_mutex);
+            if (topics.find(topic) == topics.end()) return;
+            t = topics[topic].get();
+        }
+
+        {
+            lock_guard<mutex> lock(subscriber_mutex);
+            if (subscribers.find(sub) == subscribers.end()) return;
+            s = subscribers[sub].get();
+        }
+
+        t->subscribe(s);
     }
 
     void publish(const string& msg, const string& topic) {
-        if (topics.find(topic) == topics.end()) return;
+        Topic* t = nullptr;
+
+        {
+            lock_guard<mutex> lock(topic_mutex);
+            if (topics.find(topic) == topics.end()) return;
+            t = topics[topic].get();
+        }
 
         Message m(msg);
-        topics[topic]->publish(m);
+        t->publish(m);
     }
 };
 
@@ -104,6 +179,8 @@ int main() {
 
     p.publish("news", "Hello World");
     p.publish("news", "Another Update");
+
+    this_thread::sleep_for(chrono::seconds(1)); // allow worker to process
 
     return 0;
 }
